d9d5275fb0a9249fec9aa5e23c8afc825869fb4e
HenryBenaye <henry.be@outlook.com>
2020-05-07 11:04:17 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 console.log(document.location);
 console.log(document.doctype);
 console.log("Document dingen doen");
+function myFunction() {
+    var text = document.querySelector("#text").innerText = "All the elements";
+}   
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
+boxes.forEach((e) => {
+    e.style.color = 'blue';
+    e.style.font = 'calibri';
+})
